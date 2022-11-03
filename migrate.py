ff17edc18b6afa10cb2741a1fec81b1174d7a17d
rajabh121 <117347950+rajabh121@users.noreply.github.com>
2022-11-03 15:49:29 +0530

    Add files via upload

---
+++ migrate.py
@@ -0,0 +1,54 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+import sqlalchemy
+
+
+# connect_tcp_socket initializes a TCP connection pool
+# for a Cloud SQL instance of MySQL.
+def connect_tcp_socket() -> sqlalchemy.engine.base.Engine:
+    # Note: Saving credentials in environment variables is convenient, but not
+    # secure - consider a more secure solution such as
+    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+    # keep secrets safe.
+    db_host = "10.116.137.1"
+    db_user = "root"
+    db_pass = "abhishek"
+    db_name = "abhishek"
+    db_port = 3306
+
+    pool = sqlalchemy.create_engine(
+        # Equivalent URL:
+        # mysql+pymysql://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
+        sqlalchemy.engine.url.URL.create(
+            drivername="mysql+pymysql",
+            username=db_user,
+            password=db_pass,
+            host=db_host,
+            port=db_port,
+            database=db_name,
+        ),
+        # ...
+    )
+    return pool
+
+res=connect_tcp_socket()
+print(res)
+
+query = """
+CREATE DATABASE testDB;
+"""
+res.execute(query)
